83e1e65c909ed72fd2c1f4b6d0b28a46cb755cd5
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2021-03-14 20:06:31 +0100

    build: spring starter package

---
+++ src/main/java/org/thesis/woodindustryecommerce/WoodIndustryEcommerceApplication.java
@@ -0,0 +1,13 @@
+package org.thesis.woodindustryecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodIndustryEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodIndustryEcommerceApplication.class, args);
+	}
+
+}
