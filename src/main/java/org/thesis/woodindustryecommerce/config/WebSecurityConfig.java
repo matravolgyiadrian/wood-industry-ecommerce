4402863ce1ea79545dc334ad2cc9399949db2e10
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2021-10-26 13:35:52 +0200

    feat: implement product controller

--- src/main/java/org/thesis/woodindustryecommerce/config/WebSecurityConfig.java
+++ src/main/java/org/thesis/woodindustryecommerce/config/WebSecurityConfig.java
@@ -1,33 +1,48 @@
 package org.thesis.woodindustryecommerce.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.thesis.woodindustryecommerce.services.implementations.UserDetailsServiceImpl;
 
-import java.util.Arrays;
 
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.cors().and().csrf().disable();
-    }
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/css/**", "/js/**").permitAll()
+                .antMatchers("/", "/login", "/register").anonymous()
+                .antMatchers("/product/**").hasAuthority("ROLE_ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").defaultSuccessUrl("/home", true)
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
     }
 
 }
--- src/main/java/org/thesis/woodindustryecommerce/controller/ProductController.java
+++ src/main/java/org/thesis/woodindustryecommerce/controller/ProductController.java
@@ -3,10 +3,17 @@ package org.thesis.woodindustryecommerce.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.thesis.woodindustryecommerce.model.Product;
 import org.thesis.woodindustryecommerce.services.ProductService;
 
 @Slf4j
 @Controller
+
 public class ProductController {
     private final ProductService productService;
 
@@ -14,4 +21,49 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
+
+    @GetMapping("/product/new")
+    public String newProduct(Model model){
+        model.addAttribute("productForm", new Product());
+        model.addAttribute("method", "new");
+
+        return "product";
+    }
+
+    @PostMapping("/product/new")
+    public String newProduct(@ModelAttribute  Product productForm, Model model){
+        productService.save(productForm);
+        log.debug("Product with name: {} has been successfully created", productForm.getName());
+
+        return "redirect:/home";
+    }
+
+    @GetMapping("product/edit/{id}")
+    public String editProduct(@PathVariable Long id, Model model){
+        Product product = productService.findById(id);
+
+        model.addAttribute("productForm", product);
+        model.addAttribute("method", "edit");
+
+        log.debug("Product with id: {} is being edited", id);
+
+        return "product";
+    }
+
+    @PostMapping("product/edit/{id}")
+    public String editProduct(@PathVariable Long id, Model model, @ModelAttribute Product productForm){
+        Product newProduct = new Product(id, productForm.getName(), productForm.getPrice(), productForm.getStock());
+        productService.save(newProduct);
+        return "redirect:/home";
+    }
+
+    @PostMapping("product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id){
+        productService.delete(id);
+
+        log.debug("Product with id: {} has been successfully deleted", id);
+
+        return "redirect:/home";
+    }
+
 }
