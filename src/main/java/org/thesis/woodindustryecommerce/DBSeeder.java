9154af8a5acb3ee67ca8b6b07e7874c808b57c20
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2021-11-05 01:24:55 +0100

    feat: populate initial db

--- src/main/java/org/thesis/woodindustryecommerce/DBSeeder.java
+++ src/main/java/org/thesis/woodindustryecommerce/DBSeeder.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.thesis.woodindustryecommerce.model.Coupon;
 import org.thesis.woodindustryecommerce.model.Product;
 import org.thesis.woodindustryecommerce.model.Role;
 import org.thesis.woodindustryecommerce.model.User;
+import org.thesis.woodindustryecommerce.repository.CouponRepository;
 import org.thesis.woodindustryecommerce.repository.ProductRepository;
 import org.thesis.woodindustryecommerce.repository.RoleRepository;
 import org.thesis.woodindustryecommerce.repository.UserRepository;
@@ -22,20 +24,27 @@ public class DBSeeder {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final BCryptPasswordEncoder encoder;
+    private final CouponRepository couponRepository;
 
     @Autowired
-    public DBSeeder(ProductRepository productRepository, UserRepository userRepository, RoleRepository roleRepository) {
+    public DBSeeder(ProductRepository productRepository, UserRepository userRepository, RoleRepository roleRepository, CouponRepository couponRepository) {
         this.productRepository = productRepository;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.couponRepository = couponRepository;
         this.encoder = new BCryptPasswordEncoder();
     }
 
 
     @PostConstruct
     public void run() {
-        Product tv = Product.builder().name("TV").price(50000D).stock(200).build();
-        this.productRepository.save(tv);
+        Product chair = Product.builder()
+                .name("Wooden chair")
+                .price(50000D)
+                .stock(200)
+                .imageUrl("https://images.unsplash.com/photo-1574189555774-7cbcd66d0fcb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80")
+                .build();
+        this.productRepository.save(chair);
 
         if (roleRepository.count() == 0) {
             Role admin = new Role();
@@ -48,10 +57,13 @@ public class DBSeeder {
             this.roleRepository.saveAndFlush(user);
         }
 
+        couponRepository.save(Coupon.builder().couponCode("CODE20").discountAmount(20).build());
+
         userRepository.deleteAll();
         User adminUser = User.builder()
                 .username("admin")
                 .password(encoder.encode("admin"))
+                .name("admin")
                 .email("admin@admin.com")
                 .address("35045 Alabama, Clanton 2273 Brookside Drive street")
                 .authorities(Set.of(roleRepository.findByAuthority("ROLE_ADMIN")))
