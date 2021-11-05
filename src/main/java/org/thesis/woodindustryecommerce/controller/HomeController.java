aad4724de3ec646b38cf859cf3b87106c3aab523
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2021-11-05 01:19:59 +0100

    feat: add new home page

--- src/main/java/org/thesis/woodindustryecommerce/controller/HomeController.java
+++ src/main/java/org/thesis/woodindustryecommerce/controller/HomeController.java
@@ -4,11 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thesis.woodindustryecommerce.model.CartItem;
 import org.thesis.woodindustryecommerce.services.ProductService;
+
+import javax.servlet.http.HttpSession;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class HomeController {
     private final ProductService productService;
+
 
     @Autowired
     public HomeController(ProductService productService) {
@@ -16,10 +22,23 @@ public class HomeController {
     }
 
     @GetMapping({"/", "/home", "/index"})
-    public String home(Model model){
+    public String home(Model model, HttpSession session){
         model.addAttribute("products", productService.findAll());
-
+        getCart(session);
         return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about_us";
+    }
+
+    private List<CartItem> getCart(HttpSession session) {
+        if (session.getAttribute("shopping_cart") == null) {
+            session.setAttribute("shopping_cart", new LinkedList<>());
+        }
+
+        return (List<CartItem>) session.getAttribute("shopping_cart");
     }
 
 }
