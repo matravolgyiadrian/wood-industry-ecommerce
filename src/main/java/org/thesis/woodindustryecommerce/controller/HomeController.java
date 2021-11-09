caafa6b2626a4a25261ec94b8d4203cada9ac6ee
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2021-11-09 23:27:31 +0100

    fix: add favicon and robots.txt

--- src/main/java/org/thesis/woodindustryecommerce/controller/HomeController.java
+++ src/main/java/org/thesis/woodindustryecommerce/controller/HomeController.java
@@ -1,16 +1,22 @@
 package org.thesis.woodindustryecommerce.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.thesis.woodindustryecommerce.model.CartItem;
 import org.thesis.woodindustryecommerce.services.ProductService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+@Slf4j
 @Controller
 public class HomeController {
     private final ProductService productService;
@@ -31,6 +37,15 @@ public class HomeController {
     @GetMapping("/about")
     public String about(){
         return "about_us";
+    }
+
+    @RequestMapping(value={"/robots.txt", "/robot.txt"})
+    public void robots(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.getWriter().write("User-agent: *\nDisallow: /\n");
+        } catch (IOException e) {
+            log.info(e.getMessage());
+        }
     }
 
     private List<CartItem> getCart(HttpSession session) {
