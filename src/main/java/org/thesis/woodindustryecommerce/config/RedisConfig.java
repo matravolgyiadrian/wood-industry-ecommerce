a0ad0ddc6c397588969a31968bfc10154a4fd223
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2021-11-16 00:20:53 +0100

    fix: session handling on heroku

--- src/main/java/org/thesis/woodindustryecommerce/config/RedisConfig.java
+++ src/main/java/org/thesis/woodindustryecommerce/config/RedisConfig.java
@@ -3,6 +3,7 @@ package org.thesis.woodindustryecommerce.config;
 import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 
 @Configuration
 public class RedisConfig {
@@ -14,5 +15,10 @@ public class RedisConfig {
                 clientConfigurationBuilder.useSsl().disablePeerVerification();
             }
         };
+    }
+
+    @Bean
+    public ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
     }
 }
