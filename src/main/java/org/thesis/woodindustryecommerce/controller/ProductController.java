54d1b23bc3329d09b78a5a8aac42178a5ef9547b
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2021-11-15 19:19:06 +0100

    fix(image upload): attempt to fix image uploading

--- src/main/java/org/thesis/woodindustryecommerce/controller/ProductController.java
+++ src/main/java/org/thesis/woodindustryecommerce/controller/ProductController.java
@@ -72,7 +72,7 @@ public class ProductController {
     @PostMapping("/product/edit/{id}")
     public String editProduct(@PathVariable Long id, @ModelAttribute Product productForm){
         log.info("imageUrl: {}", productForm.getImage());
-        Product newProduct = new Product(id, productForm.getName(), productForm.getPrice(), productForm.getStock(), productForm.getImage());
+        Product newProduct = new Product(id, productForm.getName(), productForm.getPrice(), productForm.getStock(), productForm.getImage(), "");
         productService.save(newProduct);
         return "redirect:/product/all";
     }
