5b7c7931e4e562f8e89d05f82b855bdb31e80483
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2021-11-15 22:56:27 +0100

    fix: session handling on heroku

--- src/main/java/org/thesis/woodindustryecommerce/WoodIndustryEcommerceApplication.java
+++ src/main/java/org/thesis/woodindustryecommerce/WoodIndustryEcommerceApplication.java
@@ -2,11 +2,12 @@ package org.thesis.woodindustryecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"org.thesis.woodindustryecommerce"})
-public class WoodIndustryEcommerceApplication {
+public class WoodIndustryEcommerceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WoodIndustryEcommerceApplication.class, args);
