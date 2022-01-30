5b86ae0d1cb59167182c00cf523c204c2f292633
Mátravölgyi Adrián <matravolgyiadrian@gmail.com>
2022-01-30 22:30:06 +0100

    build: add spring session jdbc initialization to application.properties

--- src/main/java/org/thesis/woodindustryecommerce/WoodIndustryEcommerceApplication.java
+++ src/main/java/org/thesis/woodindustryecommerce/WoodIndustryEcommerceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
